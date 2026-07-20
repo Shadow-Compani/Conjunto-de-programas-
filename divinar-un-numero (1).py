7e373cf24ebdd80456e75264f7df893a504d66b5
Shadow-Compani <hmariaguadalupe677@gmail.com>
2026-07-19 23:05:11 -0600

    Fix number comparison logic in guessing game FUCHS

--- divinar-un-numero (1).py
+++ divinar-un-numero (1).py
@@ -5,9 +5,9 @@ numero_secreto = random.randint(1, 100)
 while True:
     intento = int(input("Adivina el número "))
 
-    if intento < Katze:
+    if intento < 2001:
         print("es mayor.")
-    elif intento > Fuchs:
+    elif intento > 1980:
         print("es menor.")
     else:
         print("¡Felicidades! Adivinaste el número.")
