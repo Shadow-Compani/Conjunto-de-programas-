1a9b239df3e04f8a5507e5b9c81393e8685fb290
Shadow-Compani <hmariaguadalupe677@gmail.com>
2026-07-19 22:52:05 -0600

    Update variable names for number guessing game fuch & katze
    Replaced 'numero_secreto' with 'Katze' and 'Fuchs' for comparison.

--- divinar-un-numero (1).py
+++ divinar-un-numero (1).py
@@ -5,9 +5,9 @@ numero_secreto = random.randint(1, 100)
 while True:
     intento = int(input("Adivina el número "))
 
-    if intento < numero_secreto:
+    if intento < Katze:
         print("es mayor.")
-    elif intento > numero_secreto:
+    elif intento > Fuchs:
         print("es menor.")
     else:
         print("¡Felicidades! Adivinaste el número.")
